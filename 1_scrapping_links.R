7c9a640e12cbcde4d6ff74dbe82bd67975b2e1b3
ds3919 <79234944+ds3919@users.noreply.github.com>
2025-01-17 17:02:10 -0500

    Better Control of Number of Images Scraped
    Added upper and lower limits of how many images are scraped per class

--- 1_scrapping_links.R
+++ 1_scrapping_links.R
@@ -1,5 +1,4 @@
-
-# Instalar y cargar paquetes necesarios
+# Install and load necessary packages
 if (!require('rvest')) install.packages('rvest')
 if (!require('httr')) install.packages('httr')
 install.packages("tidyverse")
@@ -13,12 +12,12 @@ library(dplyr)
 
 print(getwd())
 
-# ARREGLA LAS IMAGENES BING DE UNA LISTA
+# FIX BING IMAGES IN A LIST
 fix_urls <- function(urls_list) {
 
-  # Recorrer los elementos de la lista de URLs
+  # Remove duplicates based on 'urls' column
   for (i in seq_along(urls_list)) {
-    # Aplicar la función de corrección y filtrado a cada URL
+    # Apply correction and filtering function to each URL
     urls_list[[i]] <- sapply(urls_list[[i]], function(url) {
       full_url <- paste0("https://www.bing.com", url)
       return(full_url)
@@ -29,22 +28,35 @@ fix_urls <- function(urls_list) {
   return(urls_list)
 }
 
-# Función para obtener URLs de imágenes de Google
-get_image_urls <- function(query, num_images = 30) {
-  url <- paste0("https://www.bing.com/images/search?q=", URLencode(query)) #paste0("https://www.google.com/search?q=", URLencode("car"))
-  webpage <- read_html(GET(url, user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")))
+# Function to get Google image URLs
+get_image_urls <- function(query) {
 
-  # Buscar las imágenes de alta resolución
-  img_nodes <- html_nodes(webpage, "a[href*='/images/search?']")#html_nodes(webpage, "a[href*='/imgres?']")
-  img_urls <- html_attr(img_nodes, "href")
+  collected_urls <- c()
+  
+  while (length(collected_urls) < 30) { # Set lower limit of images per class
+    url <- paste0("https://www.bing.com/images/search?q=", URLencode(query)) #paste0("https://www.google.com/search?q=", URLencode("car"))
+    webpage <- read_html(GET(url, user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36")))
 
-  # Filtrar URLs de imágenes válidas
-  urls_filtrados <- img_urls[grepl("^/images/search\\?view", img_urls)]
-  links_ref <- fix_urls(urls_filtrados)
-  return(links_ref)
+    # Find high resolution images
+    img_nodes <- html_nodes(webpage, "a[href*='/images/search?']")#html_nodes(webpage, "a[href*='/imgres?']")
+    img_urls <- html_attr(img_nodes, "href")
+
+    # Filter valid image URLs
+    urls_filtrados <- img_urls[grepl("^/images/search\\?view", img_urls)]
+    links_ref <- fix_urls(urls_filtrados)
+
+    collected_urls <- unique(c(collected_urls, links_ref))
+
+    Sys.sleep(sample(2:5, 1))
+  }
+  # Set upper limit of images per class
+  if (length(collected_urls) > 45) {
+    collected_urls <- collected_urls[1:45]
+  }
+  return(collected_urls)
 }
 
-# Lista de palabras
+# Word list
 # Make sure to change between spanish and english
 words <- read.csv(file.path("d:\\Internship\\WebScraper\\web_scraping_for_bing_image\\wordList.csv"), header = TRUE, fileEncoding = "Latin1")
 words
@@ -63,7 +75,7 @@ full_image_results_df
 full_image_results_unnested <- tidyr::unnest(full_image_results_df, cols = c(urls))
 full_image_results_unnested
 
-# Eliminar duplicados basándose en la columna 'urls'
+# Remove duplicates based on 'urls' column
 full_image_results_unnested_sin_duplicados <- full_image_results_unnested %>% distinct(urls, .keep_all = TRUE)
 full_image_results_unnested_sin_duplicados
 
